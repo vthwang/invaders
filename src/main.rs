7d0ac631c585b90e618c8665406946b838279fed
vthwang <vincent@fishboneapps.com>
2026-03-11 14:54:00 -0700

    feat: use audio package
    Signed-off-by: vthwang <vincent@fishboneapps.com>

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use rusty_audio::Audio;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut audio = Audio::new();
+    audio.add("explosion", "audio/original/explode.wav");
+    audio.add("lose", "audio/original/lose.wav");
+    audio.add("move", "audio/original/move.wav");
+    audio.add("pew", "audio/original/pew.wav");
+    audio.add("startup", "audio/original/startup.wav");
+    audio.add("win", "audio/original/win.wav");
+
+    audio.play("startup");
+
+    // Cleanup
+    audio.wait();
+
+    Ok(())
 }
